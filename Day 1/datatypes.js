644b5bf57b96fb77397f7e61f09a5bb5bc1fe44b
RashmiPai <68188323+RashmiPai@users.noreply.github.com>
2020-07-14 21:16:53 +0530

    Add files via upload

---
+++ Day 1/datatypes.js
@@ -0,0 +1,8 @@
+// Write a brief intro on available datatypes in javascript.
+// The various datatypes in javascript are : number , string , object.
+var num = 234; // number datatype
+console.log(num);
+var name = "abcde"; // string datatype
+console.log(name);
+var con = {start:"Beginning",end:"Ending"}; // object datatype
+console.log(con);
