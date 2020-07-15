24574d8b995227158e3e53a390caa5ae75d76d8c
RashmiPai <68188323+RashmiPai@users.noreply.github.com>
2020-07-15 17:47:12 +0530

    Add files via upload

---
+++ Day 2/ip.js
@@ -0,0 +1,6 @@
+// 'prompt' is used take user input.
+/* Prompt is a modal in javascript. Modals are those popups which don't allow the user to interact with the other part of the 
+webpage unless the user interacts with modal popup. */
+let num; // considering the variable 'num'
+num = prompt("Enter a number to continue!"); // assigning the value entered by the user in the prompt to num
+console.log(num); // the entered number is logged in the console
